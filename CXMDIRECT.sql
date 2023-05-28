59cd44846ba93313e50ca3204db77bb1bc580ff2
Maniek13 <mani3k1989@gmail.com>
2023-05-28 12:01:06 +0200

    sql tables/procedure

--- CXMDIRECT.sql
+++ CXMDIRECT.sql
@@ -1,22 +1,19 @@
 CREATE DATABASE CXMDIRECT
 GO
 
-
 USE CXMDIRECT
 GO
 
 CREATE TABLE Nodes(
 	Id INT IDENTITY(1,1) PRIMARY KEY,
 	ParentId INT,
-	NodeId HIERARCHYID,
-	NodeLvl AS NodeId.GetLevel(),
-	Name NVARCHAR(MAX) NOT NULL
+	Name NVARCHAR(MAX) NOT NULL,
+	Description NVARCHAR(MAX)
 )
 GO
 
 CREATE TABLE ExceptionLogs(
 	Id INT IDENTITY(1,1) PRIMARY KEY,
-	ExtensionId BIGINT,
 	ExtensionType NVARCHAR(MAX),
 	InstanceData DATETIME,
 	Parameters NVARCHAR(MAX),
@@ -25,15 +22,9 @@ CREATE TABLE ExceptionLogs(
 )
 GO
 
-CREATE INDEX Nodes_BreadthFirst ON Nodes(Id, NodeId, NodeLvl)
-GO
-
 CREATE PROCEDURE DeleteNode (@id INT, @error NVARCHAR(MAX) = 'OK' OUT)
 AS
-	DECLARE @NodeId HIERARCHYID = (SELECT NodeId FROM Nodes WHERE Id = @id)
-
-
-	IF (((SELECT @NodeId.GetDescendant(null, null)) IS NOT NULL) AND (SELECT Id FROM Nodes WHERE NodeId = @NodeId.GetDescendant(null, null)) IS NOT NULL )
+	IF EXISTS (SELECT Id FROM Nodes WHERE ParentId = @id)
 	BEGIN
 		SET @error =  'First need to delete all child nodes'
 	END
@@ -44,35 +35,23 @@ AS
 GO	
 
 
-CREATE PROCEDURE AddNode(@parentId INT, @name NVARCHAR(MAX), @error NVARCHAR(MAX) OUT)
+CREATE PROCEDURE AddNode(@parentId INT, @name NVARCHAR(MAX), @description NVARCHAR(MAX) = '', @error NVARCHAR(MAX) OUT)
 AS
 	DECLARE @parentHierarchyId HIERARCHYID
 
 	IF EXISTS (SELECT ParentId FROM Nodes WHERE Id = @parentId) OR @parentId = 0
 	BEGIN
-		IF(@parentId = 0)
+		IF(@parentId < 0)
 		BEGIN
-			SET @parentHierarchyId = '/'
+			SET @error = 'Parrent Id must be grater or equel 0'
 		END
 		ELSE
 		BEGIN
-			SET @parentHierarchyId = (SELECT NodeId FROM Nodes WHERE Id = @parentId)
+			INSERT INTO Nodes (ParentId, Name, Description) VALUES
+			(@parentId, @name, @description)
+			SET @error = @@IDENTITY
 		END
 
-		DECLARE @lastChild HIERARCHYID;
-
-		INS_ER:
-
-			SELECT @lastChild = MAX(NodeId) 
-				FROM Nodes 
-				WHERE NodeId.GetAncestor(1) = @parentHierarchyId
-
-			INSERT INTO Nodes (NodeId, Name, ParentId) 
-				SELECT @parentHierarchyId.GetDescendant(@lastChild, null), @name, @parentId
-
-			SET @error = @@IDENTITY
-
-		IF @@ERROR <> 0 GOTO INS_ER
 	END
 	ELSE
 	BEGIN
